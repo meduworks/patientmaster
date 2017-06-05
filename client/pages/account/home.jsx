be904de6028d670ac63aa2d543c07b649b55da0f
Jithendranath Gupta Y <guptaa.pavan@gmail.com>
2017-06-05 12:37:51 +0530

    Added Basic information

--- client/pages/account/home.jsx
+++ client/pages/account/home.jsx
@@ -3,6 +3,7 @@ const Moment = require('moment');
 const React = require('react');
 
 
+
 class HomePage extends React.Component {
     constructor(props) {
 
@@ -43,67 +44,84 @@ class HomePage extends React.Component {
     render() {
 
         return (
-            <section className="section-home container">
-                <div className="row">
-                    <div className="col-sm-7">
-                        <h1 className="page-header">My account1</h1>
-                        <div className="row">
-                            <div className="col-sm-4">
-                                <div className="well text-center">
-                                    <div className="stat-value">
-                                        {this.state.hour}
-                                    </div>
-                                    <div className="stat-label">hour</div>
+                <section className="section-home container">
+                <div className="col-xs-12 col-sm-12">    
+                    <h2>Basic Information</h2>
+                    <br/>
+                    <br/>
+                        <form  className='form-horizontal'>
+                            <div className="form-group">
+                                 <div className="col-xs-1 col-md-1 text-right">
+                                    <label htmlFor="usr" >Name:</label>
+                                </div>
+                                <div className="col-sm-8 col-md-8">
+                                    <input type="text" className="form-control" id="usr"/>
                                 </div>
                             </div>
-                            <div className="col-sm-4">
-                                <div className="well text-center">
-                                    <div className="stat-value">
-                                        {this.state.minute}
-                                    </div>
-                                    <div className="stat-label">minute</div>
+                            <div className="form-group">
+                                <div className="col-xs-1 col-md-1 text-right">
+                                    <label htmlFor="occupation">Occupation:</label>
+                                </div>
+                                <div className="col-sm-8 col-md-8">
+                                    <input type="text" className="form-control" id="occupation"/>
                                 </div>
                             </div>
-                            <div className="col-sm-4">
-                                <div className="well text-center">
-                                    <div className="stat-value">
-                                        {this.state.second}
-                                    </div>
-                                    <div className="stat-label">second</div>
+                            <div className="form-group">
+                                <div className="col-xs-2 col-md-1 text-right">
+                                    <label htmlFor="usr">Sex:</label>
+                                </div>
+                            <div className="col-sm-8 col-md-8">
+                                    <label className="radio-inline">
+                                        <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/> Male
+                                    </label>
+                                    <label className="radio-inline">
+                                        <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/> Female
+                                    </label>
+                                    <label className="radio-inline">
+                                        <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"/> Others
+                                    </label>
                                 </div>
                             </div>
-                            <div className="col-sm-4">
-                                <div className="well text-center">
-                                    <div className="stat-value">
-                                        {this.state.year}
+                            <div className="form-group">
+                                <div className="col-xs-1 col-md-1 text-right">
+                                        <label htmlFor="occupation">Allergies:</label>
                                     </div>
-                                    <div className="stat-label">year</div>
+                                <div className="col-sm-4 col-md-2">
+                                    <select className="form-control">
+                                        <option>Drug</option>
+                                        <option>Food</option>
+                                        <option>Other</option>
+                                    </select>
                                 </div>
                             </div>
-                            <div className="col-sm-4">
-                                <div className="well text-center">
-                                    <div className="stat-value">
-                                        {this.state.month}
-                                    </div>
-                                    <div className="stat-label">month</div>
+                            <div className="form-group">
+                                  <div className="col-sm-4 col-md-2 text-left">
+                                     <label htmlFor="usr">Existing conditions:</label>
+                                </div>
+                                
+                                <div className="col-sm-6 col-md-6">
+                                    <ol>
+                                    <li><textarea className="form-control" rows="1"></textarea></li>
+                                    <li><textarea className="form-control" rows="1"></textarea></li>
+                                    <li><textarea className="form-control" rows="1"></textarea></li>
+                                    <li><textarea className="form-control" rows="1"></textarea></li> 
+                                    </ol>
                                 </div>
                             </div>
-                            <div className="col-sm-4">
-                                <div className="well text-center">
-                                    <div className="stat-value">
-                                        {this.state.day}
+                            <div className="form-group">
+                                    <div className="col-sm-4 col-md-2 text-left">
+                                            <label htmlFor="usr">Major Health  Events:</label>
                                     </div>
-                                    <div className="stat-label">day</div>
-                                </div>
+                                    <div className="col-sm-6 col-md-6">
+                                        <ol>
+                                        <li><textarea className="form-control" rows="1"></textarea></li>
+                                        <li><textarea className="form-control" rows="1"></textarea></li>
+                                        <li><textarea className="form-control" rows="1"></textarea></li>
+                                        <li><textarea className="form-control" rows="1"></textarea></li> 
+                                        </ol>
+                                    </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-5">
-                        <h1 className="page-header">Throttle guage</h1>
-                        <div className="text-center">
-                            <i className="fa fa-dashboard bamf"></i>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </section>
         );
